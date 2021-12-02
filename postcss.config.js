64127f9465a426408d71e82ecbd6537ab484f522
Adam Morris <adam@kernelpanicstudios.com>
2021-12-01 22:42:38 -0600

    Completed most of the basic styling and typography.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  }
-}
+  plugins: [
+    require("postcss-import")({
+      path: ["./themes/2021-design/assets/css"],
+    }),
+    require("tailwindcss"),
+    require("autoprefixer"),
+  ],
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 module.exports = {
-  purge: ['layouts/**/*.html'],
+  purge: ["layouts/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    color: {
+      foreground: "#0a0a0a",
+      background: "#f0f0f0",
+    },
+    extend: {
+      fontFamily: {
+        body: ["source-sans-pro", "sans-serif"],
+        header: ["futura-pt", "source-sans-pro", "sans-serif"],
+        mono: [
+          "source-code-pro",
+          "Consolas",
+          "Liberation Mono",
+          "Courier",
+          "monospace",
+        ],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
